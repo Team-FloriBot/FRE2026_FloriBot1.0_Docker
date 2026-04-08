26a635cb2a000d412c74ec35c50635bd739a323f
TimoZimmermann <78686561+TimoZimmermann@users.noreply.github.com>
2026-04-08 12:57:17 +0200

    Update entrypoint.sh

--- stage/entrypoint.sh
+++ stage/entrypoint.sh
@@ -1,14 +1,7 @@
 #!/bin/bash
 set -e
 
-if [ -f /opt/venv/bin/activate ]; then
-  source /opt/venv/bin/activate
-fi
-
 source /opt/ros/jazzy/setup.bash
-
-if [ -f /ws/install/setup.bash ]; then
-  source /ws/install/setup.bash
-fi
+source /ws/install/setup.bash
 
 exec "$@"
